48e12907b5b37b8efaafb978c6c4801fcdc63570
Joao Teixeira <joao.teixeira@theloop.pt>
2024-11-20 21:40:08 +0000

    CORS Netifly

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module WeatherApp
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001'
+        origins 'http://localhost:3001', "https://weather-app-frontend-joao-teixeira.netlify.app/"
         resource '*',
                  headers: :any,
                  methods: [:get, :post, :put, :patch, :delete, :options, :head]
